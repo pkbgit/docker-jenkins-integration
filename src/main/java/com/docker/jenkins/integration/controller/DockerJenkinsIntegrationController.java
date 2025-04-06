f2feca683bd81da92c4e9f980fe80a4df6acbbc0
Prakash <prakashkbehera@gmail.com>
2025-04-06 10:51:40 +0530

    RequestMapping value changed

--- src/main/java/com/docker/jenkins/integration/controller/DockerJenkinsIntegrationController.java
+++ src/main/java/com/docker/jenkins/integration/controller/DockerJenkinsIntegrationController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/DockerJenkinsIntegration")
+@RequestMapping(value = "/DockerJenkins")
 public class DockerJenkinsIntegrationController {
 	
 	@GetMapping(value = "/welcome")
