5620a4f5bf72727e6d27ddf185d6e378ac83d1df
Prakash <prakashkbehera@gmail.com>
2025-07-16 15:17:12 +0530

    Update DockerJenkinsIntegrationController.java

--- src/main/java/com/docker/jenkins/integration/controller/DockerJenkinsIntegrationController.java
+++ src/main/java/com/docker/jenkins/integration/controller/DockerJenkinsIntegrationController.java
@@ -3,6 +3,7 @@ package com.docker.jenkins.integration.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 @RequestMapping(value = "/DockerJenkins")
