0b731cecb82edf305642e744893238385b6bf8ac
Prakash <prakashkbehera@gmail.com>
2025-08-22 21:16:00 +0530

    Junit test class updated

--- src/test/java/com/docker/jenkins/integration/controller/DockerJenkinsIntegrationControllerTest.java
+++ src/test/java/com/docker/jenkins/integration/controller/DockerJenkinsIntegrationControllerTest.java
@@ -2,11 +2,10 @@ package com.docker.jenkins.integration.controller;
 
 import org.junit.Before;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -14,12 +13,12 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+
 @SpringBootTest
-//@RunWith(SpringRunner.class)
+@AutoConfigureMockMvc
 class DockerJenkinsIntegrationControllerTest {
 	
-	
+	@Autowired
 	private MockMvc mockMvc;
 	
 	@Autowired
@@ -36,9 +35,9 @@ class DockerJenkinsIntegrationControllerTest {
 	@Test
 	public void getStringValueTest() throws Exception{
 		
-		mockMvc.perform(get("/welcome"))
+		mockMvc.perform(get("/DockerJenkins/welcome"))
 							.andExpect(status().isOk())
-							.andExpect(content().string("Welcome to Docker Jenkins Integration Application"));
+							.andExpect(content().string("Hi !!! Welcome to Docker Jenkins Integration Application."));
 		
 	}
 
